2d4b06a87b9a9f9ece201c5ff56e4d55d65e435b
helloforrestworld <helloforrestworld@163.com>
2020-02-14 12:51:07 +0800

    (webpack guide) optimization

--- guide/src/counter.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-  const div = document.createElement('div')
-  div.id = 'counter'
-  div.innerHTML = 0
-  div.addEventListener('click', () => {
-    div.innerHTML = parseInt(div.textContent, 10) + 1
-  })
-  document.body.append(div)
-}
